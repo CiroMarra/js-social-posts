35ddb38e3f216bae50564c3ff1fc14d2708acc73
CiroMarra <tensha93@gmail.com>
2024-03-25 18:06:10 +0100

    feat: add a milestone1 to main.js

--- main.js
+++ main.js
@@ -63,3 +63,55 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+// vado a stampare tutte le squadre
+
+const postContainer = document.querySelector('#container');
+posts.forEach((singlePost) => {
+    const postTemplate = generateSinglePost(singlePost);
+
+    postContainer.innerHTML += postTemplate;
+});
+
+
+// FUNCTIONS
+
+function generateSinglePost (postObject) {
+    const { id, content, media, author, likes, created} = postObject;
+
+    const postTemplate = `<div class="post">
+    <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src=${author.image} alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${author.name}</div>
+                    <div class="post-meta__time">${created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${content}</div>
+        <div class="post__image">
+        <img src=${media} alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `
+
+    return postTemplate;
+}
+
